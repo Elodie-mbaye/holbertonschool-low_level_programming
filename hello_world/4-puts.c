5e49141b3fb14779d2ffbb3e836cb17cbd683451
root <root@ElodieSeck.localdomain>
2024-10-21 12:52:58 +0200

    C Command

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include <stdio.h>
+
 /**
- * int main (void)
- {
- * puts ("Programming is like building a multilingual puzzle
+ * main - prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
  *
- * Return 0
- 
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+} 
