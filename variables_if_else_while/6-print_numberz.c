1c8a7df05923d176c4eabe6a4612c10fdb388b3f
root <root@ElodieSeck.localdomain>
2024-10-23 10:28:48 +0200

    Command C

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -6,15 +6,16 @@
  *
  * Return: Always (Success)
  */
-int main (void)
+int main(void)
 {
 	int n;
 
-	for (n = '0'; n <= '9';n++)
+	for (n = '0'; n <= '9'; n++)
 	{
 		putchar(n);
 	}
-	putchar('\n')
-	
-	return (0)
+
+	putchar('\n');
+
+	return (0);
 }
