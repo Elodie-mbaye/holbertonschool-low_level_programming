dbeb97dbbd7273fd09b1bc69c6aa3a45861ca2da
root <root@ElodieSeck.localdomain>
2024-11-08 14:46:48 +0100

    argc,argv

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argv: an array of strings containing the arguments (not use here)
+ * @argc: the number of argument
+ *
+ * Return: 0 if succeful
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc -1);
+
+	return (0);
+}
