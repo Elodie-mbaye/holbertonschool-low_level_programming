f3bdfa6a7c053c48a7254589942a27433a96f768
root <root@ElodieSeck.localdomain>
2024-11-08 14:30:42 +0100

    argc ; argv

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
-int main(int argc,char *argv[])
+/**
+ * main - displays the program name
+ * @argc: the number of arguments(not use here)
+ * @argv: an array of strings containing the arguments
+ *
+ * Return: 0 if succeful
+ */
+int main(int argc, char *argv[])
 {
 	(void)argc;
+
 	printf("%s\n", argv[0]);
 
 	return (0);
