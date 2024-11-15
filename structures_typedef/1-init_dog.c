733fa689b833285fa0769e2e8e57f79fee3b3c05
root <root@ElodieSeck.localdomain>
2024-11-15 15:26:13 +0100

    dog project

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stddef.h>
 
 /**
  * init_dog -  initialize a variable of type struct dog
@@ -11,7 +12,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d)
+	if (d == NULL)
+		return;
 	{
 		d->name = name;
 		d->age = age;
