786ec9c90be248c0f9f227d01e4ad1d89e84f41a
root <root@ElodieSeck.localdomain>
2024-11-15 18:45:19 +0100

    dog dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -16,7 +16,9 @@ struct dog
 /**
  * dot-t _ typedef for struct dog
  */
+typedef struct dog dog_t;
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
