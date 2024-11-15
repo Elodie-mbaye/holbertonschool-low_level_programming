2c71189a2d901f1b8e248a6257db3a1430914b5d
root <root@ElodieSeck.localdomain>
2024-11-15 19:57:48 +0100

    dog commit

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,30 +1,35 @@
 #include "dog.h"
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 /**
- * new_dog - creates a new dog
+ * new_dog - creates new dog
  * @name: the name of the dog
  * @age: the age of the dog
  * @owner: the owner of the dog
  *
- * Return: NULL if the function fails
+ * Return: NULL if the function is fails
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *d = malloc(sizeof(dog_t));
+	dog_t *d;
 
+	if (name == NULL || owner == NULL)
+	{
+		return (NULL);
+	}
+	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 	{
 		return (NULL);
 	}
 	d->name = malloc(strlen(name) + 1);
+
 	if (d->name == NULL)
 	{
 		free(d);
 		return (NULL);
 	}
 	strcpy(d->name, name);
-
 	d->owner = malloc(strlen(owner) + 1);
 
 	if (d->owner == NULL)
