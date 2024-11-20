617117055aa7cbd62366035706064fe7eebba79b
root <root@ElodieSeck.localdomain>
2024-11-20 12:52:49 +0100

    Command calc

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include "3-calc.h"
 #include <stdio.h>
-#include <string.h>
 /**
 * main - program to perform simple operations
 * @argc: Number of arguments
@@ -11,6 +10,7 @@
 */
 int main(int argc, char *argv[])
 {
+	int a, b, calc;
 	int (*func)(int, int);
 
 	if (argc != 4)
@@ -18,14 +18,22 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 
 	func = get_op_func(argv[2]);
-	if (func == NULL)
+	if (func == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
+	if (b == 0 && (argv[2][0] == 47 || argv[2][0] == 37))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	calc = get_op_func(argv[2])(a, b);
 
-	printf("%d\n", func(atoi(argv[1]), atoi(argv[3])));
+	printf("%d\n", calc);
 	return (0);
 }
