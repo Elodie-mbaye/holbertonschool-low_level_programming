70a04d2f258e9146c020ba9221a9aa5ac5aa6108
root <root@ElodieSeck.localdomain>
2024-11-20 09:53:29 +0100

    Command toghether task 3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,40 +1,31 @@
 #include <stdlib.h>
 #include "3-calc.h"
+#include <string.h>
+#include <stdio.h>
  /**
-  * main - Performs simple operations
+  * main - program to perform simple operations
  * @argc: Number of arguments
  * @argv: Array of arguments
  *
- * Return: 0 on success, or exit with the appropriate status code
+ * Return:exits 0 normally, 98 on argc error, 99 on illegal operator
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
-	int (*operation)(int, int);
+	int (*func)(int, int);
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-
-	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	if (argc != 4)
 	{
-		_putchar('E');
-		_putchar('r');
-		_putchar('r');
-		_putchar('o');
-		_putchar('r');
-		_putchar('\n');
-		exit(100);
+		printf("Error\n");
+		exit(98);
 	}
-	result = operation(num1, num2);
 
-	if (result < 0)
-		_putchar('-');
-	result = -result;
+	func = get_op_func(argv[2]);
+	if (func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-	if (result / 10)
-		_putchar((result / 10) + '0');
-	_putchar((result % 10) + '0');
-		_putchar('\n');
-
-		return (0);
+	printf("%d\n", func(atoi(argv[1]), atoi(argv[3])));
+	return (0);
 }
