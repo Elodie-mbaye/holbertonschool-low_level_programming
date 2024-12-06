4404d975e10a0b5d013efe46aeb67ebaf2221c4e
root <root@ElodieSeck.localdomain>
2024-12-06 13:53:30 +0100

    add node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -10,21 +10,24 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	char *dup_str;
+	int i = 0;
 
-	if (str == NULL)
+	dup_str = strdup(str);
+	if (dup_str == NULL)
 		return (NULL);
+
+	while (dup_str[i])
+		i++;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
-		return (NULL);
-
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
 	{
-		free(new_node);
+		free(dup_str);
 		return (NULL);
 	}
-	new_node->len = strlen(str);
+	new_node->str = dup_str;
+	new_node->len = i;
 	new_node->next = *head;
 	*head = new_node;
 
