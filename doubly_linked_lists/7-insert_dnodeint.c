9d4c937505d62f57a321bbd0db5bfba0b3556a5d
root <root@ElodieSeck.localdomain>
2024-12-06 13:19:05 +0100

    insers dnodeint

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,28 +10,38 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tmp = *h, *new_node;
+	dlistint_t *new_node, *tmp;
+	unsigned int i = 0;
 
-	while (idx != 1)
+	if (h == NULL)
+		return (NULL);
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	tmp = *h;
+	while (tmp && i < idx - 1)
 	{
 		tmp = tmp->next;
-		if (!tmp)
-			return (NULL);
-		idx--;
+		i++;
 	}
+	if (tmp == NULL)
+		return (NULL);
 
-	if (!tmp->next)
+	if (tmp->next == NULL && i == idx - 1)
 		return (add_dnodeint_end(h, n));
 
 	new_node = malloc(sizeof(dlistint_t));
 
-	if (!new_node)
+	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
-	new_node->prev = tmp;
 	new_node->next = tmp->next;
-	tmp->next->prev = new_node;
+	new_node->prev = tmp;
+
+	if (tmp->next != NULL)
+		tmp->next->prev = new_node;
 	tmp->next = new_node;
 
 	return (new_node);
